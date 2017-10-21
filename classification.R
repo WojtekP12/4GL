6bb37616102fcff52e14a59f8892c01e06558f41
IzabelaPabich <189742@edu.p.lodz.pl>
2017-10-21 17:16:13 +0200

    Add files via upload

--- classification.R
+++ classification.R
@@ -15,12 +15,14 @@ iris_data.test <- iris_data[ind==2, 1:4]
 iris_data.trainLabels <- iris_data[ind==1, 5]
 iris_data.testLabels <- iris_data[ind==2, 5]
 #uzycie algorytmu knn (zbior teningowy, zbior testowy, klasy, wspolczynnik k)
-iris_pred <- knn(train = iris_data.training, test = iris_data.test, cl = iris_data.trainLabels, k=3)
+for(k in c(1,3,7,13,17,67)) {
+iris_pred <- knn(train = iris_data.training, test = iris_data.test, cl = iris_data.trainLabels, k=k)
 #wrzucenie predykcji do tabeli
 irisTestLabels <- data.frame(iris_data.testLabels)
 #z³aczenie ze zbiorem testowym
 results <- data.frame(iris_pred, iris_data.testLabels)
 names(results) <- c("Predicted Species", "Test Data")
-results
 #wyœwietlenie wyników
+print(paste("Wyniki dla k = ", k))
 CrossTable(x = iris_data.testLabels, y = iris_pred, prop.chisq=FALSE, prop.r=FALSE, prop.c=FALSE, prop.t=FALSE)
+}
